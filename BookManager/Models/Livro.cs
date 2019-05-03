88f2ae68f3453dbc932dfb4e77e4f37e2f7e702c
Vinicius Carvalho <Vinicius.Carvalho@class-solutions.com.br>
2019-05-02 21:00:09 -0300

    Criação da classe Livro e inicio da criação do menu.

---
+++ BookManager/Models/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookManager.Models
+{
+  public class Livro
+  {
+    public static List<Livro> Livros { get; set; }
+
+    public string Nome { get; set; } 
+    public string Descricao { get; set; }
+    public int Ano { get; set; }
+    public DateTime DataLancamento { get; set; }
+    public float Largura { get; set; }
+    public float Altura { get; set; }
+    public float Profundidade { get; set; }
+    public string Dimensoes { get => $"{Largura} x {Altura} x {Profundidade}"; }
+
+    public Livro(string nome)
+    {
+      Nome = nome;
+    }
+  }
+}
--- BookManager/Program.cs
+++ BookManager/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Console;
+using static System.DateTime;
 
 namespace BookManager
 {
@@ -6,7 +8,50 @@ namespace BookManager
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      WriteLine($"BookManager Iniciados às {Now.ToShortDateString()}");
+      while (true)
+        ShowMenu();
+    }
+
+    private static void ShowMenu()
+    {
+      WriteLine("Escolha uma das opções abaixo:");
+      WriteLine("1. Adicionar um novo livro");
+      WriteLine("2. Listar livros cadastrados");
+      WriteLine("3. Remover um livro");
+
+      string opcao = ReadLine();
+
+      switch (opcao)
+      {
+        case "1":
+          AdicionarNovoLivro();
+          break;
+        case "2":
+          ListarLivros();
+          break;
+        case "3":
+          RemoveLivro();
+          break;
+        default:
+          WriteLine("Opção Inválida!");
+          break;
+      }
+    }
+
+    private static void RemoveLivro()
+    {
+
+    }
+
+    private static void ListarLivros()
+    {
+
+    }
+
+    private static void AdicionarNovoLivro()
+    {
+
     }
   }
 }
